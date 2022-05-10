698eb97f44642df425f7616bf80fb06b3d2cfe96
Niall Grainger <niallgrainger@gmail.com>
2022-05-09 21:21:54 -0700

    Added expo location

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+ const [errorMessage, setErrorMessage] =useState(null)
+
+useEffect(() => {
+  load()
+}, [])
+async function load() {
+  try {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+
+    if(status != 'granted') {
+      setErrorMessage('Access to location is neeeded to run the app')
+      return
+    }
+    const location = await Location.getCurrentPositionAsync()
+
+    const {latitude, longitude} = location.coords;
+
+    alert(`latitude: ${latitude}, longitude : ${longitude}`)
+  } catch (error) {
+
+  }
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Hellofffg!</Text>
       <StatusBar style="auto" />
     </View>
   );
